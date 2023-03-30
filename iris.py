28ac42c9767a8938c4c833b2a56b1f9a4224494c
keithmmc <100876230+keithmmc@users.noreply.github.com>
2023-03-30 23:05:50 +0100

    Create iris.py

---
+++ iris.py
@@ -0,0 +1,39 @@
+#importing the modlues to begin the project
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import pandas as pd
+import sys
+
+
+cols = ['sepal_Length', 'sepal_Width','Petal_Length','Petal_Width','Class']
+csv = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+#reading the CSV file 
+iris =  pd.read_csv(csv, names = cols)
+#returning the first ten rows of data to ensure that the file has loaded correctly
+print(iris.head(10))
+print(iris.tail(10)) 
+
+
+#now that i have return both rows to ensure the file was loaded correctly. I want to test the datatype
+print("the columns in this dataframe are as follows n: ")
+print(iris.dtypes)
+
+ 
+
+###going to print the above information to a data file
+original_stdout = sys.stdout 
+
+##open the file iris.txt and begin to print the info
+with open("iris.txt", "a") as f:
+    sys.stdout = f 
+    
+    print('\n************************ ANALYSIS OF THE IRIS DATASET *************************\n\n') 
+    print(iris.head(10))
+    print(iris.tail(10))
+    print(iris.dtypes)
+    print(f"The iris DataFrame has {iris.ndim} dimensions.\n")
+    print(f"The Iris data set consists of {iris.shape[0]} rows and {iris.shape[1]} columns corresponding to the rows and columns of the csv file.\n")
+    print(f"The iris DataFrame has {iris.size} elements in total.\n")
+    print(f" The index of the DataFrame is: ", iris.index)
